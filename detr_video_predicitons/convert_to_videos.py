19df7fde71538653f3dd10c141e15f14f90a4f7e
chamathabeysinghe <abeysinghechamath@gmail.com>
2021-07-26 22:46:06 +0530

    convert_to_videos

--- detr_video_predicitons/convert_to_videos.py
+++ detr_video_predicitons/convert_to_videos.py
@@ -3,26 +3,39 @@ import os
 import glob
 
 
-in_dir = "/home/cabe0006/mb20_scratch/chamath/detr-v3/evaluation_results/videos"
-out_dir = "/home/cabe0006/mb20_scratch/chamath/detr-v3/evaluation_results/videos_mp4"
-os.makedirs(out_dir, exist_ok=True)
+in_dir_base = "/home/cabe0006/mb20_scratch/chamath/data/evaluation_27/predictions/{}"
+out_dir_base = "/home/cabe0006/mb20_scratch/chamath/data/evaluation_27/prediction_videos/{}"
 
 
-def write_file(vid_frames, file_name):
+def write_file(vid_frames, vid_file_name):
     height, width, layers = vid_frames[0].shape
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter(file_name, fourcc, 6.0, (width, height))
+    out = cv2.VideoWriter(vid_file_name, fourcc, 6.0, (width, height))
     for i in range(len(vid_frames)):
         out.write(vid_frames[i])
 #         out.write((vid_frames[i]).astype(np.uint8))
     out.release()
 
 
-file_names = ["CU25L1B1In", "CU25L2B1Out", "OU50B1L1In", "OU50B1L1Out"]
-for file_name in file_names:
-    print(file_name)
-    files = sorted(glob.glob(os.path.join(in_dir, f"{file_name}*")))
-    images = [cv2.imread(f) for f in files]
-    write_file(images, os.path.join(out_dir, f'{file_name}.mp4'))
+dirs = ['detr_v4_75']
 
+file_names = [
+    "CU15L1B1In_1", "CU15L1B1Out_1",
+    "CU15L1B4In_1", "CU15L1B4Out_1",
+    "CU25L1B4In_1", "CU25L1B4Out_1",
+    "CU10L1B5In_1", "CU10L1B5Out_1",
+    "OU10B1L1In_1", "OU10B1L1Out_1",
+    "OU10B3L3In_1", "OU10B3L3Out_1",
+    "OU50B1L2In_1", "OU50B1L2Out_1",
+    "OU50B2L1In_1", "OU50B2L1Out_1",
+]
+for d in dirs:
+    in_dir = "/home/cabe0006/mb20_scratch/chamath/data/evaluation_27/predictions/{}".format(d)
+    out_dir = "/home/cabe0006/mb20_scratch/chamath/data/evaluation_27/prediction_videos/{}".format(d)
+    os.makedirs(out_dir, exist_ok=True)
+    for file_name in file_names:
+        print(file_name)
+        files = sorted(glob.glob(os.path.join(in_dir, f"{file_name}*")))
+        images = [cv2.imread(f) for f in files]
+        write_file(images, os.path.join(out_dir, f'{file_name}.mp4'))
 
